1f7d0c22e137ff7680eb33e2760ff8653cd25de2
jessie <jessie266729@gmail.com>
2016-04-23 23:11:29 +0800

    更改提示内容

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-echo "hello world";
+echo "hello world!";
 echo "I am jessie, and I am studying php.";
 $xml = simplexml_load_file("http://localhost/test.xml"); //创建 SimpleXML对象 
 var_dump($xml);
