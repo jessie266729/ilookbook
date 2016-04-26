9268e9b42a0ac1128cd5c8ad6a28e3d99caa54a4
jessie <lixiuping@xcloud.cc>
2016-04-26 18:08:40 +0800

    add css

--- thinkj/book/Public/Home/js/index.js
+++ thinkj/book/Public/Home/js/index.js
@@ -21,3 +21,57 @@
 		}
 	};
 }();
+
+/**
+*旋转效果
+*/
+!function(){
+	var timer = 0;
+	var oTimer = 0;
+	var deg = 0;
+	var targ = $('[name="more-animate"]');
+	for (var i = 0; i < targ.length; i++) {
+		$(targ[i]).on('mouseover',aMouseOver).on('mouseout',aMouseOut);
+	};
+	function aMouseOver(){
+		var tt = $(this);
+		if(timer == 0 && oTimer == 0){
+			timer = setInterval(function(){
+				if(deg >= 180){
+					window.clearInterval(timer);
+					timer = 0;
+				}
+				else{
+					deg += 10;
+					if (deg >= 90) 
+						tt.text("...");
+					tt.css({'transform':'rotateY('+deg+'deg)','-webkit-transform':'rotateY('+deg+'deg)','-moz-transform':'rotateY('+deg+'deg)','opcity':'0.7'});
+				}
+			},50);
+		}
+	}
+	function aMouseOut(){
+		if(oTimer == 0){
+			var ott = $(this);
+			var otext = ott.attr("values");
+			oTimer = setInterval(function(){
+				if(timer == 0){
+					window.clearInterval(oTimer);
+					timer = setInterval(function(){
+						if(deg <= 0){
+							window.clearInterval(timer);
+							timer = 0;
+							oTimer = 0;
+						}
+						else{
+							deg -= 10;
+							if (deg <= 90) 
+								ott.text(otext);
+							ott.css({'transform':'rotateY('+deg+'deg)','-webkit-transform':'rotateY('+deg+'deg)','-moz-transform':'rotateY('+deg+'deg)'});
+						}
+					},50);
+				}
+			},50);
+		}
+	}
+}();
--- thinkj/book/Runtime/Cache/210fe538d46acb93e4b248f50b8ef16684e12685.file.index.html.php
+++ thinkj/book/Runtime/Cache/210fe538d46acb93e4b248f50b8ef16684e12685.file.index.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.6, created on 2016-04-25 19:51:26
+<?php /* Smarty version Smarty-3.1.6, created on 2016-04-26 18:07:33
          compiled from "D:/wamp/www/thinkj/book/Home/View\Index\index.html" */ ?>
 <?php /*%%SmartyHeaderCode:323157186a7bcb8c74-04577685%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '210fe538d46acb93e4b248f50b8ef16684e12685' => 
     array (
       0 => 'D:/wamp/www/thinkj/book/Home/View\\Index\\index.html',
-      1 => 1461585082,
+      1 => 1461665247,
       2 => 'file',
     ),
   ),
@@ -41,7 +41,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
         <div class="j-content">
             <div class="j-ad">
                 <div class="j-parts new-push">
-                    <h1>上新</h1> 
+                    <h1 class="new-floor" name="more-animate" values="上新">上新</h1> 
                     <?php  $_smarty_tpl->tpl_vars['v'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['v']->_loop = false;
  $_smarty_tpl->tpl_vars['k'] = new Smarty_Variable;
  $_from = $_smarty_tpl->tpl_vars['artic']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
@@ -51,8 +51,10 @@ $_smarty_tpl->tpl_vars['v']->_loop = true;
 ?>
                     <div class="push-content">
                         <div class="push-head">
-                            <h3><?php echo $_smarty_tpl->tpl_vars['v']->value['title'];?>
-</h3><span class="push-author"><?php echo $_smarty_tpl->tpl_vars['v']->value['author'];?>
+                            <h3><a href="<?php echo @__MODULE__;?>
+/Article/story/file_name/<?php echo $_smarty_tpl->tpl_vars['v']->value['file_name'];?>
+"><?php echo $_smarty_tpl->tpl_vars['v']->value['title'];?>
+</a></h3><span class="push-author"><?php echo $_smarty_tpl->tpl_vars['v']->value['author'];?>
 </span>
                         </div>
                         <a name="data-maxlength 150" href="<?php echo @__MODULE__;?>
@@ -61,18 +63,6 @@ $_smarty_tpl->tpl_vars['v']->_loop = true;
 </a>
                     </div>
                     <?php } ?>
- <!--                    <div class="push-content">
-                        <div class="push-head">
-                            <h3>标题</h3><span class="push-author">作者</span>
-                        </div>
-                        <a name="data-maxlength 150">这一场清水镇的相遇改变了所有人的命运，甚至改变了整个大荒的命运。只为贪图那一点温暖、一点陪伴，一点不知道什么时候会消散的死心塌地。相思是一杯有毒的美酒，入喉甘美，销魂蚀骨，直到入心入肺，便再也无药可解，毒发时撕心裂肺，只有心上人的笑容可解，陪伴可解，若是不得，便只余刻骨相思，至死不休</a>
-                    </div>
-                    <div class="push-content">
-                        <div class="push-head">
-                            <h3>标题</h3><span class="push-author">作者</span>
-                        </div>
-                        <a name="data-maxlength 150">这一场清水镇的相遇改变了所有人的命运，甚至改变了整个大荒的命运。只为贪图那一点温暖、一点陪伴，一点不知道什么时候会消散的死心塌地。相思是一杯有毒的美酒，入喉甘美，销魂蚀骨，直到入心入肺，便再也无药可解，毒发时撕心裂肺，只有心上人的笑容可解，陪伴可解，若是不得，便只余刻骨相思，至死不休这一场清水镇的相遇改变了所有人的命运，甚至改变了整个大荒的命运。只为贪图那一点温暖、一点陪伴，一点不知道什么时候会消散的死心塌地。相思是一杯有毒的美酒，入喉甘美，销魂蚀骨，直到入心入肺，便再也无药可解，毒发时撕心裂肺，只有心上人的笑容可解，陪伴可解，若是不得，便只余刻骨相思，至死不休</a>
-                    </div> -->
                 </div>
                 <div class="new-hr"></div>
                 <div class="new-targe">
@@ -102,40 +92,46 @@ $_smarty_tpl->tpl_vars['v']->_loop = true;
                     </ul>
                 </div>
             </div>
-            <div class="j-part-head">
-                <div>1</div><p class="m-head">诗词大全</p>
-            </div>
-            <ul class="j-parts part-poem">
-                <?php  $_smarty_tpl->tpl_vars['v'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['v']->_loop = false;
+            <div class="m-hr"></div>
+            <div class="j-floor">
+                <div class="j-part-head">
+                    <div class="m-floor" name="more-animate" values="1">1</div><p class="m-head">诗词大全</p>
+                </div>
+                <ul class="j-parts part-poem">
+                    <?php  $_smarty_tpl->tpl_vars['v'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['v']->_loop = false;
  $_smarty_tpl->tpl_vars['k'] = new Smarty_Variable;
  $_from = $_smarty_tpl->tpl_vars['poems']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['v']->key => $_smarty_tpl->tpl_vars['v']->value){
 $_smarty_tpl->tpl_vars['v']->_loop = true;
  $_smarty_tpl->tpl_vars['k']->value = $_smarty_tpl->tpl_vars['v']->key;
 ?>
-                <li><a href="<?php echo @__MODULE__;?>
+                    <li><a href="<?php echo @__MODULE__;?>
 /Pdetail/pdetail_show/p_id/<?php echo $_smarty_tpl->tpl_vars['v']->value['id'];?>
 " target="_blank"><span class="m-author">(<?php echo $_smarty_tpl->tpl_vars['v']->value['author'];?>
 )</span><span class="m-title">《<?php echo $_smarty_tpl->tpl_vars['v']->value['title'];?>
 》</span></a></li>
-                <?php } ?>
-            </ul>
-            <div class="j-part-head">
-                <div>2</div><p class="m-head">成语大全</p>
+                    <?php } ?>
+                </ul>
             </div>
-            <ul class="j-parts part-idiom">
-                <?php  $_smarty_tpl->tpl_vars['v'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['v']->_loop = false;
+            <div class="m-hr"></div>
+            <div class="j-floor">
+                <div class="j-part-head">
+                    <div class="m-floor" name="more-animate" values="2">2</div><p class="m-head">成语大全</p>
+                </div>
+                <ul class="j-parts part-idiom">
+                    <?php  $_smarty_tpl->tpl_vars['v'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['v']->_loop = false;
  $_smarty_tpl->tpl_vars['k'] = new Smarty_Variable;
  $_from = $_smarty_tpl->tpl_vars['idiom']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['v']->key => $_smarty_tpl->tpl_vars['v']->value){
 $_smarty_tpl->tpl_vars['v']->_loop = true;
  $_smarty_tpl->tpl_vars['k']->value = $_smarty_tpl->tpl_vars['v']->key;
 ?>
-                <li><a href="<?php echo $_smarty_tpl->tpl_vars['v']->value['url'];?>
+                    <li><a href="<?php echo $_smarty_tpl->tpl_vars['v']->value['url'];?>
 " target="_blank"><span class="m-title"><?php echo $_smarty_tpl->tpl_vars['v']->value['content'];?>
 </span></a></li>
-                <?php } ?>
-            </ul>
+                    <?php } ?>
+                </ul>
+            </div>
         </div>
         <footer class="j-footer">本网站提供的内容仅用于学习参考。我们不保证内容的正确性。通过使用本站内容随之而来的风险与本站无关。对任何法律问题及风险不承担任何责任。当使用本站时，代表您已接受了本站的使用条款和隐私条款。版权所有，保留一切权利。</footer>
     </body>
